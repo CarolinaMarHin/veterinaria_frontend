eea01f19dc6973d1466ffd8ed59faaf8193f15ab
Carolina Marin Hincapie - Ceiba Software <carolina.marin@ceiba.com.co>
2022-05-10 16:26:39 -0500

    Se crea prueba e2e

---
+++ e2e/src/page/producto/mascota.po.ts
@@ -0,0 +1,40 @@
+import { by, element } from 'protractor';
+
+export class MascotaPage {
+    private linkCrearMascota = element(by.id('crearMascota'));
+    private linkInputCrearMascota = element(by.id('crear-mascota'));
+    private inputNombre = element(by.id('nombre-mascota'));
+    private inputCodigo = element(by.id('codigo-mascota'));
+    private inputFecha = element(by.id('fecha-nacimiento'));
+    private inputRaza = element(by.id('raza'));
+    private inputPeso = element(by.id('peso'));
+
+    async clickBotonCrearMascota() {
+        await this.linkCrearMascota.click();
+    }
+
+    async ingresarCodigo(codigo) {
+        await this.inputCodigo.sendKeys(codigo);
+    }
+
+    async ingresarNombre(nombre) {
+        await this.inputNombre.sendKeys(nombre);
+    }
+
+    async ingresarPeso(peso) {
+        await this.inputPeso.sendKeys(peso);
+    }
+
+    async ingresarRaza(raza) {
+        await this.inputRaza.sendKeys(raza);
+    }
+
+    async ingresarFecha(fecha) {
+        await this.inputFecha.sendKeys(fecha);
+    }
+
+    async clickInputCrearMascota() {
+        await this.linkInputCrearMascota.click();
+    }
+
+}
---
+++ e2e/src/test/mascota.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { NavbarPage } from '../page/navbar/navbar.po';
+import { AppPage } from '../app.po';
+import { MascotaPage } from '../page/producto/mascota.po';
+
+describe('workspace-project Producto', () => {
+    let page: AppPage;
+    let navBar: NavbarPage;
+    let mascota: MascotaPage;
+
+    beforeEach(() => {
+        page = new AppPage();
+        navBar = new NavbarPage();
+        mascota = new MascotaPage();
+    });
+
+    it('Deberia crear mascota', () => {
+        const NOMBRE_MASCOTA = 'Canela';
+        const CODIGO_MASCOTA = '0002';
+        const PESO_MASCOTA = 23;
+        const FECHA_NACIMIENTO_MASCOTA = '2021-03-12';
+        const RAZA_MASCOTA = 'Husky';
+
+        page.navigateTo();
+        navBar.clickBotonProductos();
+        mascota.clickBotonCrearMascota();
+        mascota.ingresarNombre(NOMBRE_MASCOTA);
+        mascota.ingresarCodigo(CODIGO_MASCOTA);
+        mascota.ingresarPeso(PESO_MASCOTA);
+        mascota.ingresarRaza(RAZA_MASCOTA);
+        mascota.ingresarFecha(FECHA_NACIMIENTO_MASCOTA);
+    });
+
+    it('Deberia crear la mascota', () => {
+        page.navigateTo();
+        navBar.clickBotonProductos();
+        mascota.clickBotonCrearMascota;
+
+        expect(1).toBe(mascota.clickInputCrearMascota());
+    });
+});
