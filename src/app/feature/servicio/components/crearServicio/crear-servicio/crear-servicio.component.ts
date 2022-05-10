d9ccd36d049d8239ca6079e5a9955b450bc5c1e7
Carolina Marin Hincapie - Ceiba Software <carolina.marin@ceiba.com.co>
2022-05-10 11:23:45 -0500

    Se ajusta html crear servicio

--- src/app/feature/servicio/components/crearServicio/crear-servicio/crear-servicio.component.ts
+++ src/app/feature/servicio/components/crearServicio/crear-servicio/crear-servicio.component.ts
@@ -18,21 +18,72 @@ export class CrearServicioComponent implements OnInit {
   public listaServicios: Servicio[] = [];
   public listaVeterinarios: Veterinario[] = [];
   public listaMascotas: Mascota[] = []
+  public mostrarMensaje: Boolean = false;
+  public mostrarMensajeError: Boolean = false;
+  private totalPago: number = 0;
+  private idVeterinario: number = 0;
+  private codigoMascota: number = 0;
+  private codigoServicio: number = 0;
+  public precioServicioString: string = "$0";
 
   ngOnInit(): void {
-    this.cargarInformacion();
+    this.cargarInformacionMascotas();
+    this.cargarInformacionServicios();
+    this.cargarInformacionVeterinarios();
   }
 
-  private cargarInformacion() {
+  private cargarInformacionServicios() {
     this.servicioService.consultar().subscribe((response) => {
       this.listaServicios = response;
-    });
-    this.servicioService.consultarVeterinarios().subscribe((response) => {
-      this.listaVeterinarios = response;
-    });
-    this.mascotaService.consultar().subscribe((response) => {
-      this.listaMascotas = response;
+    },
+    error => {
+      this.mostrarMensajeError = true;
+      this.mostrarMensaje = false;
+      console.log(error); 
     });
   }
+
+  private cargarInformacionMascotas() {
+    this.mascotaService.consultar().subscribe((response) => {
+      this.listaMascotas = response;
+    },
+    error => {
+      this.mostrarMensajeError = true;
+      this.mostrarMensaje = false;
+      console.log(error); 
+    });
+  }
+
+  private cargarInformacionVeterinarios() {
+    this.servicioService.consultarVeterinarios().subscribe((response) => {
+      this.listaVeterinarios = response;
+    },
+    error => {
+      this.mostrarMensajeError = true;
+      this.mostrarMensaje = false;
+      console.log(error); 
+    });
+  }
+
+  public actualizarServicio(id:string) {
+    let servicio: Servicio = this.listaServicios.find(ser => ser.id.toString() === id.split('-')[1].trim())
+    this.precioServicioString = '$' + servicio.precioServicio;
+    this.codigoServicio = servicio.id;
+    this.totalPago = servicio.precioServicio;
+    console.log("1", this.codigoServicio, this.totalPago);
+  }
+
+  public actualizarVeterinario(id:string) {
+    let veterinario: Veterinario = this.listaVeterinarios.find(vet => vet.id.toString() === id.split('-')[1].trim());
+    this.idVeterinario = veterinario.id;
+    console.log("2", this.idVeterinario);
+  }
+
+  public actualizarMascota(id:string) {
+    let mascota: Mascota = this.listaMascotas.find(mas => mas.id.toString() === id.split('-')[1].trim())
+    this.codigoMascota = mascota.id;
+    console.log("3", this.codigoMascota);
+  }
+
 
 }
