bd60f3d83279c3090966f2d2fbc93e918cc2d04b
Carolina Marin Hincapie - Ceiba Software <carolina.marin@ceiba.com.co>
2022-05-11 12:39:35 -0500

    Pruebas unitarias

--- src/app/feature/servicio/shared/model/cita.ts
+++ src/app/feature/servicio/shared/model/cita.ts
@@ -7,13 +7,11 @@ export class Cita {
     fecha: string;
 
     constructor(id?: number, codigoMascota?: number, idVeterinario?: number, codigoServicio?: number, totalPago?: number, fecha?: string) {
-        if (id) {
             this.id = id;
             this.codigoMascota = codigoMascota;
             this.idVeterinario = idVeterinario;
             this.codigoServicio = codigoServicio;
             this.totalPago = totalPago;
             this.fecha = fecha;
-        }
     }
 }
