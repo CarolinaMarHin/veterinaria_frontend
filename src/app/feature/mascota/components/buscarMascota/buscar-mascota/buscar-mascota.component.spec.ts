51102a5ce0c0ce3cb77596f1e79134b6f603e3b0
Carolina Marin Hincapie - Ceiba Software <carolina.marin@ceiba.com.co>
2022-05-11 23:44:19 -0500

    Se actuliza y elimina mascotas con pruebas unitarias

--- src/app/feature/mascota/components/buscarMascota/buscar-mascota/buscar-mascota.component.spec.ts
+++ src/app/feature/mascota/components/buscarMascota/buscar-mascota/buscar-mascota.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { HttpService } from '@core/services/http.service';
@@ -33,5 +34,25 @@ describe('BuscarMascotaComponent', () => {
     const req = httpMock.expectOne(`${apiEndpointMascota}/1234`);
     expect(req.request.method).toBe('GET');
     req.flush(dummyMascota);
+  });
+
+  it('deberia eliminar una mascota', () => {
+    const dummyMascota = new Mascota('Mascota 1', '1234', 'Criollo', '2021-12-13', 20, 1);
+    service.eliminar(dummyMascota).subscribe((respuesta) => {
+      expect(respuesta).toEqual(true);
+    });
+    const req = httpMock.expectOne(`${apiEndpointMascota}/1`);
+    expect(req.request.method).toBe('DELETE');
+    req.event(new HttpResponse<boolean>({ body: true }));
+  });
+
+  it('deberia actualizar una mascota', () => {
+    const dummyMascota = new Mascota('Mascota 2', '1234', 'Criollo', '2021-12-13', 20, 1);
+    service.actualizar(dummyMascota).subscribe((respuesta) => {
+      expect(respuesta).toEqual(true);
+    });
+    const req = httpMock.expectOne(`${apiEndpointMascota}/1`);
+    expect(req.request.method).toBe('POST');
+    req.event(new HttpResponse<boolean>({ body: true }));
   });
 });
--- src/app/feature/mascota/components/buscarMascota/buscar-mascota/buscar-mascota.component.ts
+++ src/app/feature/mascota/components/buscarMascota/buscar-mascota/buscar-mascota.component.ts
@@ -11,32 +11,64 @@ export class BuscarMascotaComponent implements OnInit {
 
   constructor(protected mascotaService: MascotaService) { }
 
-  public mascota: Mascota = new Mascota();
+  public errorBuscarActualizarEliminar = '';
+  public exitoBuscarActualizarEliminar  = '';
+
+  public mascota: Mascota = new Mascota('', '', '', '', 0, 0);
   public mostrarMensaje = false;
   public mostrarMensajeError = false;
+  public mostrarBotonActualizarEliminar = false;
 
-  ngOnInit(): void {
-    this.iniciarObjetoMascota();
-  }
-
-  private iniciarObjetoMascota() {
-    this.mascota.nombreMascota = '';
-    this.mascota.id = 0;
-    this.mascota.peso = 0;
-    this.mascota.raza = '';
-    this.mascota.fechaNacimientoMascota = '';
-  }
-
+  ngOnInit(): void {}
   public buscarMascota(){
     this.mascotaService.consultarPorCodigo(this.mascota.codigoMascota).subscribe(mascota => {
       this.mascota = mascota;
       this.mostrarMensajeError = false;
       this.mostrarMensaje = true;
+      this.mostrarBotonActualizarEliminar = true;
+      this.exitoBuscarActualizarEliminar = 'Busqueda exitosa.';
     },
     error => {
       this.mostrarMensajeError = true;
       this.mostrarMensaje = false;
+      this.mostrarBotonActualizarEliminar = false;
+      this.errorBuscarActualizarEliminar = 'Error al buscar la mascota, intente nuevamente con un código valido.';
       console.log(error);
+    });
+  }
+
+  public eliminarMascota() {
+    this.mascotaService.eliminar(this.mascota).subscribe(respuesta => {
+      this.mascota =  new Mascota('', '', '', '', 0, 0);
+      this.exitoBuscarActualizarEliminar = 'Se elimino correctamente la mascota';
+      this.mostrarMensajeError = false;
+      this.mostrarMensaje = true;
+      this.mostrarBotonActualizarEliminar = false;
+      console.log(respuesta);
+    },
+    error => {
+      console.log('respuesta', error);
+      this.errorBuscarActualizarEliminar = 'Error al eliminar la mascota. Validar que no cuente con citas asignadas.';
+      this.mostrarMensajeError = true;
+      this.mostrarMensaje = false;
+      this.mostrarBotonActualizarEliminar = false;
+    });
+  }
+
+  public actualizarMascota() {
+    this.mascotaService.actualizar(this.mascota).subscribe(respuesta => {
+      this.exitoBuscarActualizarEliminar = 'Se actualizo correctamente la mascota';
+      this.mostrarMensajeError = false;
+      this.mostrarMensaje = true;
+      this.mostrarBotonActualizarEliminar = true;
+      this.buscarMascota();
+    },
+    error => {
+      console.log('respuesta', error);
+      this.errorBuscarActualizarEliminar = 'Ocurrio un error actualizando la mascota.';
+      this.mostrarMensajeError = true;
+      this.mostrarMensaje = false;
+      this.mostrarBotonActualizarEliminar = true;
     });
   }
 }
